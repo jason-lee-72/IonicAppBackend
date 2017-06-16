48cec48f1881d424aaba7491abbdbbd7960e0a8c
Jason Lee <jason.x.lee@accenture.com>
2017-06-16 15:53:34 +1000

    fix reconnect

--- database.js
+++ database.js
@@ -1,27 +1,27 @@
 var mongoose = require('mongoose');
 
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open');
+});
+// If the connection throws an error
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
+
 module.exports = {
     connect: function (connString) {
-        let connectWithRetry = function () {
-            mongoose.connection.on('connected', function () {
-                console.log('Mongoose default connection open to ' + connString);
-            });
-            // If the connection throws an error
-            mongoose.connection.on('error', function (err) {
-                console.log('Mongoose default connection error: ' + err);
-            });
-            // When the connection is disconnected
-            mongoose.connection.on('disconnected', function () {
-                console.log('Mongoose default connection disconnected');
-            });
-            // If the Node process ends, close the Mongoose connection 
-            process.setMaxListeners(1);
-            process.on('SIGINT', function () {
-                mongoose.connection.close(function () {
-                    console.log('Mongoose default connection disconnected through app termination');
-                    process.exit(0);
-                });
-            });
+        let connectWithRetry = function () {            
             mongoose.connect(connString, function (err) {
                 if (err) {
                     console.error(`Mongoose failed to connect to mongo (${connString}) on startup - retrying in 5 sec`);
